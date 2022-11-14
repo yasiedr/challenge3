a6b09635020a17eb37546b325e3596fcd41b5c21
yasiedr <116052273+yasiedr@users.noreply.github.com>
2022-11-13 19:22:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var confirmLower;
 var confirmUpper;
 var confirmNumber;
 var confirmSpecial;
-var userChoices;
+
 
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
